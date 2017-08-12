ffbfe69351aaef48a7a8fe8b76730ccbc9c34da1
黄滚 <renlu.xu@taobao.com>
2017-08-12 22:37:56 +0800

    pasted supported

--- src/NvwaCommon/Misc/UploadHelper.php
+++ src/NvwaCommon/Misc/UploadHelper.php
@@ -63,6 +63,62 @@ class UploadHelper
         Log::debug("file." . $item . " 不存在");
         return false;
     }
+    public function pasteUpload(Request $request, $item, $subDirectory = '')
+    {
+        $file =$request->get('file');
+        $seg = explode(";",$file);
+        if(sizeof($seg)!=2){
+            return resError(400,"wrong paramater" );
+        }
+        $segments = explode(",",$seg[1]);
+        if(sizeof($segments)!=2){
+            return resError(400,"wrong paramter");
+        }
+        $data = $segments[1];
+        $encoding = $segments[0];
+
+        $header = explode(":",$seg[0]);
+        if(sizeof($header)!=2){
+            return resError(400,"wrong paramter");
+        }
+
+        $mime = explode("/",$header[1]);
+        if(sizeof($header)!=2){
+            return resError(400,"wrong paramter");
+        }
+        $extName =  strtolower($mime[1]);
+        if(!in_array($extName,array("jpg","bmp","gif","png"))) {
+            return resError(403,"bad extension name of the file");
+        }
+        try{
+            $real_data = base64_decode($data);
+        }catch(Exception $e){
+            return resError(402,"parse data failed");
+        }
+        $y= date("Ym");
+        if (!$subDirectory) {
+            $subDirectory = date("Ym/dH/");
+        }
+        $destDirectory = $this->getUploadDirectory() . $subDirectory;
+        if (!file_exists($destDirectory)) {
+            mkdir($destDirectory, 0777, true);
+        }
+        $extension = $extName;
+        $mime = $header[1];
+        $filename = $this->buildPasteFileName($request, $extension);
+        file_put_contents($destDirectory.$filename,$real_data);
+        $clientSize = filesize($destDirectory.$filename);
+        return [
+            "filename" => $filename,
+            "extension" => $extension,
+            "mime" => $mime,
+            "filesize" => $clientSize,
+            "url" => $subDirectory . $filename
+        ];
+    }
+    function resError($code,$msg){
+        return ['code'=>$code,'msg'=>$msg];
+    }
 
     private function getUploadDirectory()
     {
@@ -73,6 +129,11 @@ class UploadHelper
     {
         $fi = microtime(true) . rand(99999, 999999);
         $extension = $request->file($item)->guessExtension();
+        return $fi . "." . $extension;
+    }
+    private function buildPasteFileName(Request $request, $extension)
+    {
+        $fi = microtime(true) . rand(99999, 999999);
         return $fi . "." . $extension;
     }
 }
